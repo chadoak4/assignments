52e5350b0bdd7aaf301708bbedd8ebe85ad5ebb9
Chad Oakley <chad.oakley@tampabay.rr.com>
2015-08-24 17:37:09 -0400

    Update day_one.rb

--- day_one.rb
+++ day_one.rb
@@ -1,10 +1,10 @@
 # 1. Assign "Hello World" to a variable named message.
-helloWorld = "Hello World"
+hello_world = "Hello World"
 # 2. Assign a different string to a different variable.
 hi = "Hello Chad!"
 # 3. Assign a number to a variable.
 n = 8
 # 4. Make an array of at least four of your favorite movies or books or bands.
-movies = ["Lion King, "Beauty and the Beast", "Finding Nemo", "Toy Story"]
+movies = ["Lion King", "Beauty and the Beast", "Finding Nemo", "Toy Story"]
 # 5. Make a hash of information about yourself with at least four key-value pairs.
 chad = {"height" => "5'11", "weight" => "185 pounds", "eye color" => "blue", "age" => "26"}
