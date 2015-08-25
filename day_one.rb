3bd9bf64fb5c7370bd52bdaa359ef8e8ddedbb9b
Chad Oakley <chad.oakley@tampabay.rr.com>
2015-08-25 01:22:45 -0400

    Update day_one.rb
    Updated # 1 and 4 + Bonuses

--- day_one.rb
+++ day_one.rb
@@ -8,3 +8,9 @@ n = 8
 movies = ["Lion King", "Beauty and the Beast", "Finding Nemo", "Toy Story"]
 # 5. Make a hash of information about yourself with at least four key-value pairs.
 chad = {"height" => "5'11", "weight" => "185 pounds", "eye color" => "blue", "age" => "26"}
+# 6. Use string interpolation to display the number from exercise 3 in the middle a string.
+puts "I have #{n} dogs"
+# 7. Make an array of hashes containing more information about your favorite movies. The hashes should have at least three key value pairs.
+stars = [{:title => "Lion King", :character => "Simba", :animal => "Lion"}, {:title =>"Beauty and the Beast", :character => "Belle", :animal => "Person"}, {:title => "Finding Nemo", :character => "Nemo", :animal => "Fish"}, {:title => "Toy Story", :character => "Woody", :animal => "Toy"}]
+# 8. Use `each` to loop through the answer from exercise #7 and print only one property from the hash. i.e., given `{ title: "Gone with the Wind" }` you print "Gone with the Wind
+stars.each{|x| puts x[:animal]}
